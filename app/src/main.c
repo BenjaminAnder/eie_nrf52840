1b5b1f281ed46ea8f3f4b8632dc40b430fa30900
BenjaminAnder <professor.dragoon@gmail.com>
2025-10-02 19:43:19 -0600

    Make 3 LEDs blink

--- app/src/main.c
+++ app/src/main.c
@@ -6,8 +6,12 @@
 #include <zephyr/drivers/gpio.h>
 
 #define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
+static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(LED2_NODE, gpios);
 
 int main(void)
 {
@@ -15,14 +19,23 @@ int main(void)
 
     if (!gpio_is_ready_dt(&led0)) 
         return -1;
+    if (!gpio_is_ready_dt(&led1)) 
+        return -1;
+    if (!gpio_is_ready_dt(&led2)) 
+        return -1;
 
     ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
+    ret = gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+    ret = gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+
     if (ret < 0)
         return ret;
 
     while (1)
     {
         gpio_pin_toggle_dt(&led0);
+        gpio_pin_toggle_dt(&led1);
+        gpio_pin_toggle_dt(&led2);
 
         k_msleep(1000);
     }
